cb4772533496f23748ffb22be0ec4e64d3b766ff
zhanchi5 <zhanchi5@outlook.com>
2016-10-21 00:16:48 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,2 +1,12 @@
+
 #include <calculator.hpp>
 #include <catch.hpp>
+
+SCENARIO("calculator sum", "[sum]") {
+    int x = 3;
+    int y = 4;
+    
+    int rv = sum( x, y );
+    
+	REQUIRE( rv == 7);
+}
