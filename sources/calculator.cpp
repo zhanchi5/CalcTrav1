900ad395c71c884a10f236a55ada17b13a82f6ea
zhanchi5 <zhanchi5@outlook.com>
2016-10-25 12:23:16 +0400

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -33,7 +33,7 @@ long double square_root(float a) {
     int i=0;
     while(1) {
         b=pow1;
-        pow1=0.5*(pow1 a/pow1); 
+        pow1=0.5*(pow1+a/pow1); 
         if (pow1=b) {
             i++; if (i>1);
             break;
