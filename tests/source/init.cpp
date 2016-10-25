d0325ad7ea5ecfb6e54d2878afe5e19c9bf2ab0d
zhanchi5 <zhanchi5@outlook.com>
2016-10-25 10:40:27 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -8,41 +8,41 @@
     
     float rv = summary(a,b);
     
-      REQUARE( rv == 7.0);
+      REQUIRE( rv == 7.0);
   }
    SCENARIO("calculator sub", "[sub]") {
      float a = 5.0;
      float b = 2.0;
      
      float rv = sub(a,b);
-      REQUARE (rv ==3.0);
+      REQUIRE (rv ==3.0);
    }
   SCENARIO("calculator multiplication", "[multiplication]") {
      float a = 5.0;
      float b = 2.0;
      
      float rv = multiplication(a,b);
-      REQUARE (rv ==10.0);
+      REQUIRE (rv ==10.0);
    }
   SCENARIO("calculator division", "[division]") {
      float a = 5.0;
      float b = 2.0;
      
      float rv = division(a,b);
-      REQUARE (rv ==2.5);
+      REQUIRE (rv ==2.5);
    }
   SCENARIO("calculator involution", "[involution]") {
      float a = 5.0;
      float b = 2.0;
      
      float rv = involution(a,b);
-      REQUARE (rv ==25.0);
+      REQUIRE (rv ==25.0);
    }
   /*SCENARIO("calculator square_root", "[square_root]") {
      float a = 5.0;
      float b = 2.0;
      
      float rv = sub(a,b);
-      REQUARE (rv ==3.0);
+      REQUIRE (rv ==3.0);
    }
 */
