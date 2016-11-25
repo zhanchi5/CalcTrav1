49459f335ee3f85eee8866a13250317479f876d9
zhanchi5 <zhanchi5@outlook.com>
2016-11-25 12:18:46 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -3,44 +3,44 @@
 #include <catch.hpp>
 
   SCENARIO("calculator summary", "[summary]") {
-    float a = 2.0;
-    float b = 5.0;
+    double a = 2.0;
+    int b = 5;
     
-    float rv = summary(a,b);
+    double rv = summary(a,b);
     
       REQUIRE( rv == 7.0);
   }
    SCENARIO("calculator sub", "[sub]") {
-     float a = 5.0;
-     float b = 2.0;
+     double a = 5.0;
+     int b = 2;
      
-     float rv = sub(a,b);
+     double rv = sub(a,b);
       REQUIRE (rv ==3.0);
    }
   SCENARIO("calculator multiplication", "[multiplication]") {
-     float a = 5.0;
-     float b = 2.0;
+     double a = 5.0;
+     int b = 2.0;
      
-     float rv = multiplication(a,b);
+     double rv = multiplication(a,b);
       REQUIRE (rv ==10.0);
    }
   SCENARIO("calculator division", "[division]") {
-     float a = 5.0;
-     float b = 2.0;
+     double a = 5.0;
+     int b = 2.0;
      
-     long double rv = *division(a,b);
+      double rv = *division(a,b);
       REQUIRE (rv ==2.5);
    }
   SCENARIO("calculator involution", "[involution]") {
-     float a = 5.0;
-     float b = 2.0;
+     double a = 5.0;
+     int b = 2.0;
      
-     float rv = involution(a,b);
+     double rv = involution(a,b);
       REQUIRE (rv ==25.0);
    } 
 
     SCENARIO ("calculator square-root", "[square_root]"){
-    float x = 0.25;
-    double long rv = square_root (x);
-REQUIRE ( rv == 0.5 );
+    double x = 0.25;
+    double  rv = square_root (x);
+    REQUIRE ( rv == 0.5 );
    }
