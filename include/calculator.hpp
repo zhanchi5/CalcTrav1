cddd79c0fbfc7e3ec032a89a5b38cfc444d9f3b6
zhanchi5 <zhanchi5@outlook.com>
2016-11-25 11:33:44 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,5 +1,5 @@
         double summary(float a, float b);
-        double division(float a, float b);
+        double *division(float a, float b);
         double multiplication(float a, float b);
         double sub(float a, float b);
         double involution(float a, float b);
